9d7b8bbe6c3127f80a2f68c5c372f6a57be4af47
kkawayuki <kyukiokawa@gmail.com>
2024-11-17 20:50:52 -0800

    3

--- main.cpp
+++ main.cpp
@@ -8,10 +8,10 @@
 #include <time.h>
 using namespace std;
 
-//function prototypes
-void runDay(deque<Car>&);
-void carLeaves(deque<Car>&);
-void carJoins(deque<Car>&);
+// function prototypes
+void runDay(deque<Car> &);
+void carLeaves(deque<Car> &);
+void carJoins(deque<Car> &);
 void printAll(deque<Car>);
 
 /************************************************
@@ -19,26 +19,37 @@ void printAll(deque<Car>);
  ************************************************/
 int main()
 {
-    srand(time(0)); //seed random
-    const int SIZE = 2; 
-    deque<Car> tollBooth (SIZE); //declare dequeue with intial size 2
-
-    //there are now 4 toll booths (dequeues)
-    //add more lanes
-    deque<Car>lanes[4]; 
-
-    //cout initial
-    cout << "Initial queue:\n"; 
-    printAll(tollBooth);
-
-    for(int i = 1; tollBooth.size() != 0; i++)
+    srand(time(0)); // seed random
+    const int SIZE = 2, LANES = 4;
+    
+    // add more lanes
+    deque<Car> lanes[LANES];
+    for (int i = 0; i < LANES; i++) //populate 
     {
-        cout << "Time: " << i << " Operation: ";
-        runDay(tollBooth); 
-
-        cout << "Queue:\n"; 
-        printAll(tollBooth);
+        for (int i = 0; i < SIZE; i++)
+        {
+            Car *temp = new Car; 
+            lanes[i].push_back(*temp);
+        }
     }
+
+    //updated print implementation
+    cout << "Initial queue:\n";
+    for(int i = 0; i < LANES; i++)
+    {
+        cout << "Lane " << i+1 << ":\n"; 
+        printAll(lanes[i]);
+    }
+    
+
+    // for (int i = 1; tollBooth.size() != 0; i++)
+    // {
+    //     cout << "Time: " << i << " Operation: ";
+    //     runDay(tollBooth);
+
+    //     cout << "Queue:\n";
+    //     printAll(tollBooth);
+    // }
 }
 
 /************************************************
@@ -48,33 +59,30 @@ int main()
  * Parameters: &tollBooth, as it calls other functions
  * that push/pop Car objects from the deque
  ************************************************/
-void runDay(deque<Car>&tollBooth)
+void runDay(deque<Car> &tollBooth)
 {
-    int rand1 = (rand()%100)+1; //random variable deciding addition/payment
-    if(rand1 <= 55)
+    int rand1 = (rand() % 100) + 1; // random variable deciding addition/payment
+    if (rand1 <= 55)
     {
         cout << "Car paid: ";
-        tollBooth[0].print(); 
+        tollBooth[0].print();
         carLeaves(tollBooth);
     }
-    else //the other 45%
+    else // the other 45%
     {
         cout << "Joined lane: ";
         carJoins(tollBooth);
-        tollBooth[tollBooth.size()-1].print(); //print the last car to enter deque
-
-
-        
+        tollBooth[tollBooth.size() - 1].print(); // print the last car to enter deque
     }
 }
 
 /************************************************
- * Function: Simply removes the frontmost car in 
+ * Function: Simply removes the frontmost car in
  * line
- * Parameters: &tollBooth, as a modification to the 
+ * Parameters: &tollBooth, as a modification to the
  * original deque is made.
  ************************************************/
-void carLeaves(deque<Car>&tollBooth)
+void carLeaves(deque<Car> &tollBooth)
 {
     tollBooth.pop_front();
 }
@@ -82,30 +90,30 @@ void carLeaves(deque<Car>&tollBooth)
 /************************************************
  * Function: Adds a new car to the deque
  *
- * Parameters: &tollBooth, as a modification to the 
+ * Parameters: &tollBooth, as a modification to the
  * original deque is made.
  ************************************************/
-void carJoins(deque<Car>&tollBooth)
+void carJoins(deque<Car> &tollBooth)
 {
-    Car *temp = new Car(); //use default constructor 
-    tollBooth.push_back(*temp); 
+    Car *temp = new Car(); // use default constructor
+    tollBooth.push_back(*temp);
 }
 
 /************************************************
  * Function: Adds a new car to the deque
  *
- * Parameters: tollBooth, passed by value, as no 
+ * Parameters: tollBooth, passed by value, as no
  * modificaiton to the original deque needs to be
- * made, helps guard against accidental changes. 
+ * made, helps guard against accidental changes.
  ************************************************/
-void printAll(deque<Car>tollBooth)
+void printAll(deque<Car> tollBooth)
 {
-    for(int i = 0; i != tollBooth.size(); i++)
+    for (int i = 0; i != tollBooth.size(); i++)
     {
-        cout << '\t'; //formatting
+        cout << '\t'; // formatting
         tollBooth[i].print();
     }
-    if(tollBooth.size() == 0) //handles case of empty deque
+    if (tollBooth.size() == 0) // handles case of empty deque
     {
         cout << "\tEmpty";
     }
